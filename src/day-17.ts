aaf0341ba29afe67025f30aa0547048de1082c2c
Alex Grad <git@grad.dev>
2024-12-17 12:56:38 +0400

    fix formatting

--- src/day-17.ts
+++ src/day-17.ts
@@ -8,16 +8,14 @@ const compose =
     f: (x: A) => B,
     g: (x: B) => C,
     h: (x: C) => D
-  ) => (a: A): D => (
-    h(g(f(a)))
-  );
+  ) => (a: A) :  D => h(g(f(a)));
 
 const upperCase = <T extends string>(x: T) => x.toUpperCase() as Uppercase<T>;
 const lowerCase = <T extends string>(x: T) => x.toLowerCase() as Lowercase<T>;
 const firstChar = <T extends string>(x: T) => x[0] as FirstChar<T>;
-const firstItem = <T extends unknown[]>(x: T): T[0] => x[0];
-const makeTuple = <T>(x: T): [T] => [x];
-const makeBox = <T>(value: T): { value: T } => ({ value });
+const firstItem = <T extends unknown[]>(x: T) => x[0] as T[0];
+const makeTuple = <T>(x: T) => [x];
+const makeBox = <T>(value: T) => ({ value });
 
 /**
  * TESTS
